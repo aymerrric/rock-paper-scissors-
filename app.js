b65b73101f61ac06f7ff2d9bba9b458d216d645e
aymerrric <aymeric-sabrie@orange.fr>
2025-02-12 20:31:45 +0100

    Implemant the game logic

--- app.js
+++ app.js
@@ -19,7 +19,76 @@ function getHumanChoice(){
             return humanChoice;
         }
         else{
-            alert('Please enter a correct choice between scissors, rock and paper')
+            alert('Please enter a correct choice between scissors, rock and paper');
         }
     }
 }
+
+function roundOfTheGame(){
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+    if (computerChoice === humanChoice){
+        alert('Tie');
+        return;
+    }
+    else{
+        switch (computerChoice){
+            case 'rock':
+                if (humanChoice == 'paper'){
+                    alert('You win a point');
+                    return 1;
+                } 
+                else{
+                    alert('You lost');
+                    return 0;
+                }
+            case 'paper':     
+                if (humanChoice == 'scissors'){
+                    alert('You win a point');
+                    return 1;
+                } 
+                else{
+                    alert('You lost');
+                    return 0;
+                }
+            case 'scissors':
+                if (humanChoice == 'rock'){
+                    alert('You win a point');
+                    return 1;
+                } 
+                else{
+                    alert('You lost');
+                    return 0;
+                }
+            }
+        }
+    }
+
+
+function gameLoop(){
+
+    for (let i=0; i<5; i++){
+        score = roundOfTheGame()
+        if (score == 1){
+            humanScore ++;
+        }
+        else if (score==0){
+            computerScore ++
+        }
+    }
+    if (humanScore > computerScore){
+        alert("You won")
+    }
+    else if (computerScore > humanScore){
+        alert('You lost')
+    }
+    else{
+        alert('There is a tie')
+    }
+}
+
+
+let humanScore = 0
+let computerScore = 0
+
+gameLoop()
