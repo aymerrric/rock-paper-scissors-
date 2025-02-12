4b207c6e1ea2fa6707f00b7737525246dc08fdc6
aymerrric <aymeric-sabrie@orange.fr>
2025-02-12 18:43:26 +0100

    Implement the user choice selection

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 function getComputerChoice(){
-    number = Math.floor(Math.random()*3) // return a random int number between 0 and 2
+    let number = Math.floor(Math.random()*3); // return a random int number between 0 and 2
     if (number == 0){
-        return "paper"
+        return "paper";
     }
     else if (number == 1){
-        return "scissors"
+        return "scissors";
     }
     else{
-        return 'rock'
+        return 'rock';
     }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice(){
+    while (true){
+        humanChoice = prompt('scissors, rock or paper');
+        humanChoice = humanChoice.toLowerCase() // let less error go through
+        if (humanChoice == "scissors" || humanChoice == 'rock' || humanChoice == "paper"){
+            return humanChoice;
+        }
+        else{
+            alert('Please enter a correct choice between scissors, rock and paper')
+        }
+    }
+}
